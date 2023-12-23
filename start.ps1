b5e91d47a0cc9732c483963f508703642753e858
Kostiantyn Korniienko <kostiantyn.korniienko@trilogy.com>
2023-12-23 18:04:06 +0200

    wip

--- start.ps1
+++ start.ps1
@@ -1,1 +1,3 @@
-Start-Process -FilePath "yarn" -ArgumentList "run start" -WindowStyle Hidden -NoNewWindow
+# Start-Process -FilePath "yarn" -ArgumentList "run start" -WorkingDirectory "$PSScriptRoot" #-WindowStyle Hidden
+Start-Process -FilePath "powershell" -ArgumentList "-Command", "yarn run start" -WorkingDirectory "$PSScriptRoot" -WindowStyle Hidden
+# Start-Process -FilePath "powershell" -ArgumentList "-NoExit", "-Command", "yarn run start" -WorkingDirectory "$PSScriptRoot"
