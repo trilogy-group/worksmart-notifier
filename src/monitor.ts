7776ef08639b45b2b1ea1c31bd00d8a5c967b0c1
Kostiantyn Korniienko <kostiantyn.korniienko@trilogy.com>
2023-12-23 16:07:25 +0200

    basics

--- src/monitor.ts
+++ src/monitor.ts
@@ -2,7 +2,7 @@ import notifier from 'node-notifier';
 import { exec } from 'child_process';
 
 // Replace 'YOUR_PROCESS_NAME' with the name of the process you want to monitor
-const processName = 'YOUR_PROCESS_NAME';
+const processName = 'Crossover.exe';
 
 function checkProcess() {
   exec(`tasklist`, (err, stdout, stderr) => {
@@ -13,15 +13,18 @@ function checkProcess() {
 
     if (!stdout.includes(processName)) {
       notifier.notify({
-        title: 'Process Monitor',
-        message: `${processName} has stopped running.`,
+        title: 'WorkSmart Alert',
+        message: `WorkSmart has stopped running!`,
+        sound: true,
+
       });
 
       // Stop checking once the process has stopped
-      clearInterval(interval);
+      // clearInterval(interval);
     }
   });
 }
 
 // Check every 30 seconds
-const interval = setInterval(checkProcess, 30000);
+// const interval = setInterval(checkProcess, 30000);
+const interval = setInterval(checkProcess, 1000);
